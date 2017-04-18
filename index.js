be0dbd3f58f3abfb92db632572bd4e974e1ace25
m4l1c3 <jon@jonsomnia.com>
2017-04-18 07:49:15 -0600

    Updating the poc to work with haveibeenpwned api

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 
 let http = require('http'),
     fs = require('fs'),
-    server = http.createServer((req, res) => {
-            displayForm(res);
-    }),
-    port = 1185;
-
-function displayForm(res) {
-    fs.readFile('form.html', (err, data) => {
-        res.writeHead(200, {
-           'Content-Type' : 'text/html',
-           'Content-Length': data.length
-        });
-
-        res.write(data);
-        res.end();
+    port = 1185,
+    express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    pwnd = require('haveibeenpwned')(),
+    app = express();
+    app.listen(port, () => {
+        console.log('server listening on port: ' + port);
     });
-}
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-server.listen(port);
-console.log('server listening on port: ' + port);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'form.html'));
+});
+
+app.post('/checkpasswordagainstapi', (req, res) => {
+    if(req.body !== 'undefined' && req.body.email !== '') {
+        let email = req.body.email;
+        
+        pwnd.breachedAccount(email, (err, data) => {
+            if(!err) {
+                if(data.length)
+                    return res.send({"Pwnd": true});
+                else
+                    return res.send({"Pwnd": false});
+            }
+            return res.send({"Pwnd": true});
+        });
+    }
+});
